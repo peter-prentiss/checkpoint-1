7f10bed3a3d22e327df410bfd359519f3299adc6
Peter Prentiss <peter.prentiss@gmail.com>
2017-06-26 13:40:15 -0500

    complete code challenge 1

---
+++ client.js
@@ -0,0 +1,18 @@
+let counter = 0;
+
+$(document).ready(function() {
+  $('.container').append('<button>Generate</button>');
+  $('button').click(function() {
+    console.log('working');
+    $('.container').append('<div id="new-div' + counter + '"></div>');
+    $('#new-div' + counter+'').append('<p>' + counter + '</p>')
+    .append('<button id="swap">Swap</button><button id="delete">Delete</button>');
+      counter++;
+  });
+  $('.container').on('click', '#swap', function() {
+    $(this).parent().css('background-color', 'yellow');
+  });
+  $('.container').on('click', '#delete', function() {
+    $(this).parent().remove();
+  })
+});
